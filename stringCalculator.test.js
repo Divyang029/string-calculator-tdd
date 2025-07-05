4d67b0bdf4fc68311a70523c9150cf4714f85c36
Divyang Dhameliya <dhameliyadivyang@gmail.com>
2025-07-05 12:56:25 +0530

    test: add comma-saperated two numbers '1,3' returns 4

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -7,3 +7,7 @@ test("return 0 for empty string", () => {
 test("returns number if only one number is provided", () => {
     expect(add("1")).toBe(1);
 });
+
+test("add comma-separated two numbers", () => {
+  expect(add("1,3")).toBe(4);
+});
