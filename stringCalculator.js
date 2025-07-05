34db664f21427054ef19bcb3b6a7fa182d1e96f5
Divyang Dhameliya <dhameliyadivyang@gmail.com>
2025-07-05 13:59:29 +0530

    feat: detect and throw exception on negative number

--- stringCalculator.js
+++ stringCalculator.js
@@ -11,6 +11,12 @@ function add(numbers){
     }
         
     const nums = numbers.split(delimiters).map(Number);
+    const negatives = nums.filter(n => n<0);
+
+    if(negatives.length > 0){
+        throw new Error("negative numbers not allowed: " + negatives.join(", "));
+    }
+
     return nums.reduce((sum,val) => sum+val, 0);
 }
 
