5d3c9cfb8c0af9f8634ddc1d8e5880dab4629b33
Divyang Dhameliya <dhameliyadivyang@gmail.com>
2025-07-05 13:00:02 +0530

    feat: add comma-saperated two numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,10 @@
 function add(numbers){
     if(numbers === "") return 0;
-    else return parseInt(numbers);
+    else if(!numbers.includes(",")) return parseInt(numbers);
+    else{
+        const nums = numbers.split(",").map(Number);
+        return nums.reduce((sum,val) => sum+val, 0);
+    }
 }
 
 module.exports = add;
