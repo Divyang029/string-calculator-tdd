033a9818b706aa30df9c747f6e4c162acbb19a0d
Divyang Dhameliya <dhameliyadivyang@gmail.com>
2025-07-05 13:22:22 +0530

    test: add support for custom delimeter

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -19,3 +19,7 @@ test("adds multiple numbers", () => {
 test("supports newline as delimiter", () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+test('supports custom delimiter', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
