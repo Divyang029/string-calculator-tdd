e94800f6c4a830db3216d04c9b0fda4d75b37996
Divyang Dhameliya <dhameliyadivyang@gmail.com>
2025-07-05 12:47:11 +0530

    test: return single number if one value given

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -2,4 +2,8 @@ const add = require('./stringCalculator');
 
 test("return 0 for empty string", () => {
     expect(add("")).toBe(0);
-})
+});
+
+test("returns number if only one number is provided", () => {
+    expect(add("1")).toBe(1);
+});
