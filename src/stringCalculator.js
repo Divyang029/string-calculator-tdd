ca5a3d6a3ee70225c364df3b7ea59e51f78b2ada
Divyang Dhameliya <dhameliyadivyang@gmail.com>
2025-07-05 15:22:47 +0530

    feat: can handle custom delimeter of any length

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,24 +1,37 @@
-function add(numbers){
-    if(numbers === "") return 0;
+function escapeRegExp(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function add(numbers) {
+    if (numbers === "") return 0;
 
     let delimiters = /,|\n/;
-    
-    if(numbers.startsWith("//")){
-        const match = numbers.match(/^\/\/(.+)\n(.*)$/s);
 
-        delimiters = new RegExp(match[1]);
-        numbers = match[2];
+    if (numbers.startsWith("//")) {
+        const bracketed = numbers.match(/^\/\/(.+)\n(.*)$/s);
+
+        if (bracketed) {
+            const all = bracketed[1];
+            const delimiter = [...all.matchAll(/\[(.*?)\]/g)].map(d => escapeRegExp(d[1]));
+            delimiters = new RegExp(delimiter.join("|"));
+            numbers = bracketed[2];
+        }
+        else {
+            const match = numbers.match(/^\/\/(.+)\n(.*)$/s);
+            delimiters = new RegExp(match[1]);
+            numbers = match[2];
+        }
     }
-        
-    const nums = numbers.split(delimiters).map(Number);
-    const negatives = nums.filter(n => n<0);
 
-    if(negatives.length > 0){
+    const nums = numbers.split(delimiters).map(Number);
+    const negatives = nums.filter(n => n < 0);
+
+    if (negatives.length > 0) {
         throw new Error("negative numbers not allowed: " + negatives.join(", "));
     }
 
-    const filtered = nums.filter(n => n<=1000);
-    return filtered.reduce((sum,val) => sum+val, 0);
+    const filtered = nums.filter(n => n <= 1000);
+    return filtered.reduce((sum, val) => sum + val, 0);
 }
 
 module.exports = add;
